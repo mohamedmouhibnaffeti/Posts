6b08f3b609d9dcaa15f9ae6ff1d5e12538680003
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 18:03:58 +0100

    Fixed issue

--- social-backend/models/PostModel.js
+++ social-backend/models/PostModel.js
@@ -1,7 +1,7 @@
 const {models, model, Schema} = require('mongoose')
 
 const PostSchema = Schema({
-
+    
 })
 
-const Post = models.Post model('Post', PostSchema)
+const Post = models.Post || model('Post', PostSchema)
