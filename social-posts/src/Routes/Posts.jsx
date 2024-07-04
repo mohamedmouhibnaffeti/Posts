d4ad1510320c7e2d2e0629f7ed813ddfd5d8f785
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 19:51:36 +0100

    Fixed issue

--- social-posts/src/Routes/Posts.jsx
+++ social-posts/src/Routes/Posts.jsx
@@ -8,7 +8,7 @@ export default function Posts(){
                         <span className="UserIcon"> M N </span>
                         <span> Mouhib Naffeti </span>
                     </div>
-                    <div style={{backgroundColor: "red", borderRadius: "10px", outline: "none", color: "white", paddingTop: "2px", paddingBottom: "4px", paddingLeft: "6px", paddingRight: "6px"}}>
+                    <div style={{backgroundColor: "red", borderRadius: "10px", outline: "none", color: "white", paddingTop: "2px", paddingBottom: "4px", paddingLeft: "12px", paddingRight: "12px"}}>
                         Delete
                     </div>
                 </div>
