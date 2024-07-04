e383cdca038aca485006b7aae8ac6439a4c98568
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 20:21:26 +0100

    Fixed issue

--- social-posts/src/Routes/Posts.jsx
+++ social-posts/src/Routes/Posts.jsx
@@ -1,6 +1,7 @@
+import { useState } from 'react'
 import '../CSS/Posts.css'
 export default function Posts(){
-    const [err]
+    const [err, setErr] = useState()
     const fetchPosts = async () => {
         const response = await fetch('http://localhost:3000/api/posts/all')
         if(response.ok){
