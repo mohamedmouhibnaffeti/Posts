1945440fbf03ba2cd20854f964efa70ba1001e34
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 18:52:47 +0100

    Fixed issue

--- social-backend/controllers/postsController.js
+++ social-backend/controllers/postsController.js
@@ -26,7 +26,7 @@ module.exports.createPost = async(req, res) => {
 
 module.exports.deletePost = async(req, res) => {
     try{
-
+        
     }catch(err){
         return res.status(500).json('Error creating post')
     }
