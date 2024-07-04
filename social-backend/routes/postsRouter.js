3915cc2c7e238c41062a6b57e21a384db1f4c3fc
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 18:39:11 +0100

    Fixed issue

--- social-backend/routes/postsRouter.js
+++ social-backend/routes/postsRouter.js
@@ -3,6 +3,6 @@ const PostController = require('../controllers/postsController.js')
 const router = express.Router()
 
 router.get('/all', PostController.getAllPosts)
-router.post('/')
+router.post('/', PostController.createPost)
 
 module.exports = router
