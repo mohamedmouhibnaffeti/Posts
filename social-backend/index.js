326d69d697a508f7bef5a45710e3c71b39719483
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 18:25:25 +0100

    Fixed issue

--- social-backend/index.js
+++ social-backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const postRouter = ('./routes/postsRouter.js')
+const PostRouter = ('./routes/postsRouter.js')
 const app = express()
 
 app.use(express.json())
 
-app.use("/api/posts", router)
+app.use("/api/posts", PostRouter)
 
 app.listen(3000, ()=>{
     console.log("http://localhost:3000")
