d7ee3e408d162da710ffb95fcdeab84dcf50d686
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 17:52:52 +0100

    Fixed issue

--- social-backend/index.js
+++ social-backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+import router from './routes/postsRouter'
 const app = express()
 
 app.use(express.json())
 
+app.use()
 
 app.listen(3000, ()=>{
     console.log("http://localhost:3000")
