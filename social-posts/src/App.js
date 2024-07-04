5e0cfbc88d27d21ac35216c19d396a944c25a04c
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 19:14:36 +0100

    Fixed issue

--- social-posts/src/App.js
+++ social-posts/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link }
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router"
 
 function App() {
   return (
