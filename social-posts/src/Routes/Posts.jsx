b2b1015e5df2ca61e6d7e4a8cfc1258c6419f496
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 19:36:14 +0100

    Fixed issue

--- social-posts/src/Routes/Posts.jsx
+++ social-posts/src/Routes/Posts.jsx
@@ -2,7 +2,7 @@ import '../CSS/Posts.css'
 export default function Posts(){
     return(
         <section className="postsSection">
-            <div className="posts">
+            <div className="innerPostsSection">
 
             </div>
         </section>
