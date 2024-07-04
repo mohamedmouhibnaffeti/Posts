cb5bf59dece2898f722c802a3a55a11760b08615
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 17:47:35 +0100

    Fixed issue

--- social-backend/index.js
+++ social-backend/index.js
@@ -3,7 +3,9 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/", ())
+app.get("/", (req, res)=>{
+    return res.json("Hello world")
+})
 
 app.listen(3000, ()=>{
     console.log("http://localhost:3000")
