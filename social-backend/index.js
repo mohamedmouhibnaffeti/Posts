3f2865ba49da2020272a8bcb7514b69dcac3c4a5
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 18:05:27 +0100

    Fixed issue

--- social-backend/index.js
+++ social-backend/index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use("/api/posts", Postsrouter)
+app.use("/api/posts", PostRouter)
 
 app.listen(3000, ()=>{
     console.log("http://localhost:3000")
