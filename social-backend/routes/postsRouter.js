52b99952503c8478c0522bb37447df43e538a8ad
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 17:53:17 +0100

    Fixed issue

--- social-backend/routes/postsRouter.js
+++ social-backend/routes/postsRouter.js
@@ -2,6 +2,6 @@ const express = require('express')
 const { PostsController } = require('../controllers/postsController')
 const router = express.Router()
 
-router.get("/api/posts", PostsController)
+router.get("/", PostsController)
 
 export default router
