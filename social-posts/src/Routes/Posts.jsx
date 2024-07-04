a142cf973d33aa426e2d7e20fa2f98efcb3337d7
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 20:32:40 +0100

    Fixed issue

--- social-posts/src/Routes/Posts.jsx
+++ social-posts/src/Routes/Posts.jsx
@@ -19,7 +19,25 @@ export default function Posts(){
     return(
         <section className="postsSection">
             {
-                posts.map((post, index))
+                posts.map((post, index)=>(
+                    <div className="innerPostsSection">
+                        <div className="UserHeader">
+                            <div className="innerUserHeader">
+                                <span className="UserIcon"> M N </span>
+                                <span> Mouhib Naffeti </span>
+                            </div>
+                            <div style={{backgroundColor: "red", borderRadius: "10px", outline: "none", color: "white", paddingTop: "2px", paddingBottom: "4px", paddingLeft: "12px", paddingRight: "12px", cursor: "pointer"}}>
+                                Delete
+                            </div>
+                        </div>
+                        <div className="Title">
+                            title
+                        </div>
+                        <div className="Description">
+                            description
+                        </div>
+                    </div>
+                ))
             }
         </section>
     )
