9a5616b77af80e000b93f1ac7921766543e9cf9d
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 19:22:25 +0100

    Fixed issue

--- social-posts/src/index.js
+++ social-posts/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter, Route, Link, RouterProvider} from 'react-router-dom'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
