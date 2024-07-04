3b177a6834b5d5a4c34e34fbc08c290e5e6ef0b4
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 20:32:19 +0100

    Fixed issue

--- social-posts/src/Routes/Posts.jsx
+++ social-posts/src/Routes/Posts.jsx
@@ -18,23 +18,9 @@ export default function Posts(){
     }, [])
     return(
         <section className="postsSection">
-            <div className="innerPostsSection">
-                <div className="UserHeader">
-                    <div className="innerUserHeader">
-                        <span className="UserIcon"> M N </span>
-                        <span> Mouhib Naffeti </span>
-                    </div>
-                    <div style={{backgroundColor: "red", borderRadius: "10px", outline: "none", color: "white", paddingTop: "2px", paddingBottom: "4px", paddingLeft: "12px", paddingRight: "12px", cursor: "pointer"}}>
-                        Delete
-                    </div>
-                </div>
-                <div className="Title">
-                    title
-                </div>
-                <div className="Description">
-                    description
-                </div>
-            </div>
+            {
+                posts.
+            }
         </section>
     )
 }
