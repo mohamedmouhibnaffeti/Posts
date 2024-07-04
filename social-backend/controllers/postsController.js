b508ab53a63cba5140508530b60c4afb43540abe
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 18:35:54 +0100

    Fixed issue

--- social-backend/controllers/postsController.js
+++ social-backend/controllers/postsController.js
@@ -2,4 +2,8 @@ module.exports.getAllPosts = async(req, res) => {
     return res.json("Hello World")
 }
 
-module.exports
+module.exports.createPost = async(req, res) => {
+    try{
+        
+    }
+}
