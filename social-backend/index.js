f23b9cc27730d94ddf725fad6c9bd6352c32ac5a
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 18:48:53 +0100

    Fixed issue

--- social-backend/index.js
+++ social-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const PostRouter = require('./routes/postsRouter.js')
 const mongoose = require('mongoose')
 
-mongoose.connect()
+mongoose.connect("mongodb://localhost:27017/DevDB")
 const app = express()
 
 app.use(express.json())
